4cbf54e5cf3a17b9578e7b391acc3e1096cf4a51
carsonDB <2278891315@qq.com>
2021-10-21 09:59:47 +0800

    add google-analytics 4

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -5,6 +5,20 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-XTHGK48KLD", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
